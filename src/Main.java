5e90abf7703709b5ba8191d98704011a23b115b0
Darkhan Zhursin <daratechn@gmail.com>
2024-02-23 17:52:48 -0600

    test

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Test
 //        Solution1 solution1 = new Solution1();
 //        int res = solution1.isCentered(new int[] {123});
 //        System.out.println(res);
