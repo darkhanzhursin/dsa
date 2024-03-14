9b0a87225d8133ff26e7c1169af5b89c62007e27
Darkhan Zhursin <daratechn@gmail.com>
2024-03-14 17:17:32 -0500

    lesson 10

---
+++ src/fpp/homeworks/lesson9/prob1/ArrayQueueImpl.java
@@ -0,0 +1,30 @@
+package fpp.homeworks.lesson9.prob1;
+
+public class ArrayQueueImpl {
+    private int[] arr = new int[10];
+    private int front = -1;
+    private int rear = 0;
+
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public int peek() {
+        return -1;
+    }
+
+    public void enqueue(int obj) {
+
+    }
+
+    public int dequeue() {
+        return -1;
+    }
+
+    private void resize() {}
+}
