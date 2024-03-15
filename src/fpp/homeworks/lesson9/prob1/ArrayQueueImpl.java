7674cfa29d92391e41be6be484c67323f250dd38
Darkhan Zhursin <daratechn@gmail.com>
2024-03-15 12:51:46 -0500

    hw lesson 9

--- src/fpp/homeworks/lesson9/prob1/ArrayQueueImpl.java
+++ src/fpp/homeworks/lesson9/prob1/ArrayQueueImpl.java
@@ -1,30 +1,82 @@
 package fpp.homeworks.lesson9.prob1;
 
 public class ArrayQueueImpl {
-    private int[] arr = new int[10];
-    private int front = -1;
-    private int rear = 0;
+    private int[] arr;
+    private int front;
+    private int rear;
 
-
-    public boolean isEmpty() {
-        return false;
+    public ArrayQueueImpl(int n) {
+        arr = new int[n];
+        front = -1;
+        rear = 0;
     }
 
-    public int size() {
-        return 0;
-    }
-
-    public int peek() {
-        return -1;
+    public ArrayQueueImpl() {
+        this(10);
     }
 
     public void enqueue(int obj) {
+        if (rear == arr.length - 1) {
+            resize();
+        }
+        arr[rear] = obj;
+        rear++;
+    }
 
+    public void resize() {
+        System.out.println("resizing");
+        int len = arr.length;
+        int newLen = 2 * len;
+        int[] newArray = new int[newLen];
+        System.arraycopy(arr, 0, newArray, 0, len);
+        arr = newArray;
     }
 
     public int dequeue() {
-        return -1;
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        int first = arr[++front];
+        front--;
+        moveElements(arr);
+        rear--;
+        return first;
     }
 
-    private void resize() {}
+    private void moveElements(int[] arr) {
+        int k = 0;
+        while (arr[k] != 0) {
+            arr[k] = arr[k+1];
+            k++;
+        }
+    }
+    public boolean isEmpty() {
+        return rear == 0;
+    }
+
+    public int size() {
+        return rear;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        return arr[front+1];
+    }
+
+    public static void main(String[] args) {
+        ArrayQueueImpl arrayQueue = new ArrayQueueImpl();
+        arrayQueue.enqueue(1);
+        arrayQueue.enqueue(10);
+        System.out.println("Deleted: " + arrayQueue.dequeue());
+        arrayQueue.enqueue(11);
+        System.out.println("Size: " + arrayQueue.size());
+        System.out.println("Peek: " + arrayQueue.peek());
+        arrayQueue.dequeue();
+        arrayQueue.dequeue();
+        arrayQueue.peek();
+    }
 }
---
+++ src/fpp/homeworks/lesson9/prob2/ListStackDemo.java
@@ -0,0 +1,4 @@
+package fpp.homeworks.lesson9.prob2;
+
+public class ListStackDemo {
+}
